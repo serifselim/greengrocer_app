d8cb04030bd23dd27134f6b9d9c2a11ea2e0636c
Yavuz Selim Şerifoğlu <selimserifoglu03@gmail.com>
2021-11-11 09:19:44 +0300

    get json file method added and assets uploaded

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,15 +9,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      home: HomePage()
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List _items = [];
+
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/data/items.json');
+    final data = await jsonDecode(response);
+    setState(() {
+      _items = data["items"];
+    });
+  } 
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    readJson();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Image(image: AssetImage(_items.isNotEmpty ? _items[0]["image"] : 'assets/images/cabbage.png'),),
       ),
     );
   }
